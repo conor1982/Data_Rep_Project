d38ceca9ce9712cf74b39a7e5399b9a6a63943f9
conor1982 <conor.oriordan@hotmail.com>
2021-12-06 15:16:49 +0000

    ajax not working

--- initdb.sql
+++ initdb.sql
@@ -46,13 +46,13 @@ INSERT INTO locations (loc_name, type, years_occupancy) VALUES ("Belfast", "Leas
 INSERT INTO locations (loc_name, type, years_occupancy) VALUES ("London", "Owned", 8);
 INSERT INTO locations (loc_name, type, years_occupancy) VALUES ("New York", "Leased", 2);
 
-INSERT INTO departments (dept_name, budget, location) VALUES ("Sales", 50000, 1);
+INSERT INTO departments (dept_name, budget, location) VALUES ("Sales", 500000, 1);
 INSERT INTO departments (dept_name, budget, location) VALUES ("Analytics", 100000, 1);
-INSERT INTO departments (dept_name, budget, location) VALUES ("Research", 250000, 1);
-INSERT INTO departments (dept_name, budget, location) VALUES ("IT Support", 20000, 2);
-INSERT INTO departments (dept_name, budget, location) VALUES ("Logisitcs", 55000, 2);
-INSERT INTO departments (dept_name, budget, location) VALUES ("Investment", 1000000, 4);
-INSERT INTO departments (dept_name, budget, location) VALUES ("Data Quality", 1000000, 5);
+INSERT INTO departments (dept_name, budget, location) VALUES ("Research", 25000, 1);
+INSERT INTO departments (dept_name, budget, location) VALUES ("IT Support", 200000, 2);
+INSERT INTO departments (dept_name, budget, location) VALUES ("Logisitcs", 550000, 2);
+INSERT INTO departments (dept_name, budget, location) VALUES ("Investment", 100000, 4);
+INSERT INTO departments (dept_name, budget, location) VALUES ("Data Quality", 100000, 5);
 INSERT INTO departments (dept_name, budget, location) VALUES ("Customer Support", 50000, 3);
 
 INSERT INTO employees (name, title, salary,dept) VALUES ("Conor O'Riordan","Head of Analytics",150000, 3);
@@ -60,17 +60,17 @@ INSERT INTO employees (name, title, salary,dept) VALUES ("Sean Smith","Data Scie
 INSERT INTO employees (name, title, salary,dept) VALUES ("Rachel Nolan","Sales Manager",80000, 2);
 INSERT INTO employees (name, title, salary,dept) VALUES ("Paul Lyne","Sales Analyst",50000, 2);
 INSERT INTO employees (name, title, salary,dept) VALUES ("Tom O'Connor","Sales Analyst",50000, 2);
-INSERT INTO employees (name, title, salary,dept) VALUES ("Owen McGuire","Research Lead",100000, 4);
+INSERT INTO employees (name, title, salary,dept) VALUES ("Owen McGuire","Research Lead",45000, 4);
 INSERT INTO employees (name, title, salary,dept) VALUES ("William O Donoghue","Research Lead",85000, 4);
-INSERT INTO employees (name, title, salary,dept) VALUES ("Alan Donegan","Logistics Manager",750000, 6);
-INSERT INTO employees (name, title, salary,dept) VALUES ("Miriam Lucey","Logistics Analyst",500000, 6);
-INSERT INTO employees (name, title, salary,dept) VALUES ("Sarah Tracey","Logistics Analyst",500000, 6);
-INSERT INTO employees (name, title, salary,dept) VALUES ("Colm Cooper","Head of Investment",100000, 7);
-INSERT INTO employees (name, title, salary,dept) VALUES ("Roy Keane","Investment Analyst",800000, 7);
-INSERT INTO employees (name, title, salary,dept) VALUES ("Patrick Smith","Head of Data Quality",800000, 8);
-INSERT INTO employees (name, title, salary,dept) VALUES ("Dara Moynihan","Data Quality Analyst",800000, 8);
-INSERT INTO employees (name, title, salary,dept) VALUES ("Liam Kearney","Customer Success Lead",800000, 8);
-INSERT INTO employees (name, title, salary,dept) VALUES ("Patrick O'Sullivan","Customer Success Lead",800000, 8);
+INSERT INTO employees (name, title, salary,dept) VALUES ("Alan Donegan","Logistics Manager",75000, 6);
+INSERT INTO employees (name, title, salary,dept) VALUES ("Miriam Lucey","Logistics Analyst",50000, 6);
+INSERT INTO employees (name, title, salary,dept) VALUES ("Sarah Tracey","Logistics Analyst",50000, 6);
+INSERT INTO employees (name, title, salary,dept) VALUES ("Colm Cooper","Head of Investment",10000, 7);
+INSERT INTO employees (name, title, salary,dept) VALUES ("Roy Keane","Investment Analyst",80000, 7);
+INSERT INTO employees (name, title, salary,dept) VALUES ("Patrick Smith","Head of Data Quality",66000, 8);
+INSERT INTO employees (name, title, salary,dept) VALUES ("Dara Moynihan","Data Quality Analyst",71245, 8);
+INSERT INTO employees (name, title, salary,dept) VALUES ("Liam Kearney","Customer Success Lead",80000, 8);
+INSERT INTO employees (name, title, salary,dept) VALUES ("Patrick O'Sullivan","Customer Success Lead",57000, 8);
 
 INSERT INTO users (name,password) VALUES ("admin","admin");
 INSERT INTO users (name,password) VALUES ("conor","1982");
